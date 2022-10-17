c813b3d2337faf3b03f71bfda36edd7de1bb6957
Zaidan Althafariq Raihan <zaidanraihan09@gmail.com>
2022-10-17 17:34:43 +0700

    Add project files.

---
+++ Activity2_20210140103/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity2_20210140103
+{
+    class QuickSortArray
+    {
+        static public int Partition(int[] numbers, int left, int right)
+        {
+            int pivot = numbers[left];
+            while (true)
+            {
+                while (numbers[left] < pivot)
+                    left++;
+                while (numbers[right] > pivot)
+                    right--;
+                if (left < right)
+                {
+                    int temp = numbers[right];
+                    numbers[right] = numbers[left];
+                    numbers[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        static public void SortQuick(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+                if (pivot > 1)
+                    SortQuick(arr, left, pivot - 1);
+                if (pivot + 1 < right)
+                    SortQuick(arr, pivot + 1, right);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("\nProgram mensortir angka pada Array menggunakan Quick Sorting");
+            Console.Write("\n\nMasukkan Banyaknya Elemen : ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[max];
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Input int array  : ");
+            Console.Write("\n");
+            for (int k = 0; k < max; k++)
+            {
+                Console.Write(numbers[k] + " ");
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("QuickSort menggunakan metode rekursif");
+            SortQuick(numbers, 0, max - 1);
+            for (int i = 0; i < max; i++)
+                Console.WriteLine(numbers[i]);
+            Console.ReadLine();
+        }
+    }
+}
+
